ba310f102ccecab98f1cda3190b588a6ad6ac05d
Abby-kr <abby.a.otieno@gmail.com>
2023-10-04 17:37:38 +0300

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [integer for integer in num_list if integer % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_added = [string + "!" for string in sentence_list ]
+    return exclamation_added
